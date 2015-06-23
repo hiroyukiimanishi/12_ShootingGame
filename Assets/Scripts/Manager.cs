cdf80121d2a4677732528f5d3287d8644e3b5218
hiroyukiimanishi <bohemians85@gmail.com>
2015-06-23 20:04:13 +0900

    Unity Tutorial ShootingGame Part 12
    New Commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+	// Playerプレハブ
+	public GameObject player;
+	
+	// タイトル
+	private GameObject title;
+	
+	void Start ()
+	{
+		// Titleゲームオブジェクトを検索し取得する
+		title = GameObject.Find ("Title");
+	}
+	
+	void Update ()
+	{
+
+
+#if UNITY_EDITOR
+		// ゲーム中ではなく、Xキーが押されたらtrueを返す。
+		if (IsPlaying () == false && Input.GetKeyDown (KeyCode.X)) {
+			GameStart ();
+		}
+#elif UNITY_ANDROID
+		// ゲーム中ではなく、Xキーが押されたらtrueを返す。
+		if (IsPlaying () == false && Input.GetTouch(0).phase == TouchPhase.Ended) {
+			GameStart ();
+		}
+#endif
+	}
+	
+	void GameStart ()
+	{
+		// ゲームスタート時に、タイトルを非表示にしてプレイヤーを作成する
+		title.SetActive (false);
+		Instantiate (player, player.transform.position, player.transform.rotation);
+	}
+	
+	public void GameOver ()
+	{
+		FindObjectOfType<Score>().Save();
+		// ゲームオーバー時に、タイトルを表示する
+		title.SetActive (true);
+	}
+	
+	public bool IsPlaying ()
+	{
+		// ゲーム中かどうかはタイトルの表示/非表示で判断する
+		return title.activeSelf == false;
+	}
+}
